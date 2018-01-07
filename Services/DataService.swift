85fa3b9021c9232a9ee5a887b3b59f5984497ca0
EmeraldJelly <nickb122717@icloud.com>
2018-01-07 14:36:24 -0500

    Added A HEEEP of Stuff

---
+++ Services/DataService.swift
@@ -0,0 +1,24 @@
+//
+//  DataService.swift
+//  coder-swag
+//
+//  Created by Nicholas Brewster on 1/7/18.
+//  Copyright © 2018 Nicholas Brewster. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func getCategories() -> [Category] {
+        return categories
+    }
+}
---
+++ Views/CategoryCell.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Nicholas Brewster on 1/7/18.
+//  Copyright © 2018 Nicholas Brewster. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    func updateViews(category: Category) {
+        categoryImage.image = UIImage(named: category.imageName)
+        categoryTitle.text = category.title
+    }
+}
